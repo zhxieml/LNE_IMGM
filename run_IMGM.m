f8dceabb34a8b904245ad11ccfb9a26916e1c00f
fffffarmer <fffffarmer@gmail.com>
2019-10-30 11:32:44 +0800

    update

--- run_IMGM.m
+++ run_IMGM.m
@@ -7,11 +7,11 @@ set_alg;
 global affinity target;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-varyMinGrhCnt=4;varyMaxGrhCnt=32;grhTestCnt = 5;% 
+varyMinGrhCnt=4;varyMaxGrhCnt=32;grhTestCnt = 5;%
 
 setPlotColor;
 setObsoleteVariables;% some old parameters are used for debug and other tests, less relevant to the algorithm
-% by default set to 0 for target.config.useCstInlier (affinity-driven), no use in formal test type, only useful in massiveOutlier mode 
+% by default set to 0 for target.config.useCstInlier (affinity-driven), no use in formal test type, only useful in massiveOutlier mode
 % target.config.useCstInlier = 0;% set to 1 if use consistency to mask inliers, otherwise use affinity metrics, see Sec 3.5 in PAMI paper
 % random graph test, note not the random point set test as used in mpm
 % testType: different modes for tests, e.g. formal (Fig.3&4), case(Fig.1), iter(Fig.2), massOutlier(Fig.5&6)
@@ -53,21 +53,21 @@ else
     target.config.inCntType = 'all';% set 'all' for "only a few outlier case", e.g. Fig.1&2&3&4
     target.config.category = 'deform';%'deform','outlier','density','complete'
     switch target.config.category
-        case 'deform'% same setting with 5th row in Table 1 in the PAMI paper 
+        case 'deform'% same setting with 5th row in Table 1 in the PAMI paper
             nInlier = 10;target.config.nOutlier = 0;target.config.deform = 0.15;
             target.config.density = .9;target.config.complete = 1;
             graphMinCnt = varyMinGrhCnt;graphMaxCnt = varyMaxGrhCnt;testCnt = grhTestCnt;
-        case 'outlier'% same setting with 6th row in Table 1 in the PAMI paper 
+        case 'outlier'% same setting with 6th row in Table 1 in the PAMI paper
             nInlier = 6;target.config.nOutlier = 4;target.config.deform = 0;
             target.config.density = 1;target.config.complete = 1;
             graphMinCnt = varyMinGrhCnt;graphMaxCnt = varyMaxGrhCnt;testCnt = grhTestCnt;
-        case 'density'% same setting with 7th row in Table 1 in the PAMI paper 
+        case 'density'% same setting with 7th row in Table 1 in the PAMI paper
             nInlier = 10;target.config.nOutlier = 0;target.config.deform = 0.0;
             target.config.density = 0.5;target.config.complete = 1;
             graphMinCnt = varyMinGrhCnt;graphMaxCnt = varyMaxGrhCnt;testCnt = grhTestCnt;
-        case 'complete'% same setting with 8th row in Table 1 in the PAMI paper 
+        case 'complete'% same setting with 8th row in Table 1 in the PAMI paper
             nInlier = 10;target.config.nOutlier = 0;target.config.deform = 0.05;
-            target.config.density = 1;target.config.complete = 0.1;     
+            target.config.density = 1;target.config.complete = 0.1;
     end
 end
 graphRange = graphMinCnt:4:graphMaxCnt;
@@ -92,7 +92,7 @@ iterCnt = length(iterRange);
 [~,cao_ucIdx] = ismember('cao_uc',algSet.algNameSet);
 [~,cao_pc_Idx] = ismember('cao_pc_',algSet.algNameSet);[~,cao_pc_sIdx] = ismember('cao_pc_s',algSet.algNameSet);
 [~,cao_pcIdx] = ismember('cao_pc',algSet.algNameSet);
-algCnt = length(algSet.algEnable); 
+algCnt = length(algSet.algEnable);
 X=cell(algCnt,1);
 % target.config.nodeCnt = length(target.config.selectNodeMask);
 % target.config.graphCnt = min(max(graphRange),length(target.config.selectGraphMask{1}));
@@ -136,8 +136,8 @@ listOfFeatureDataFile = dir(fullfile(pwd,'data', 'feature', cls, '*.mat'));
 
 
 %% Initial visualization
-% hAlg1 = figure('Name',sprintf('%s', alg(1).strName), 'NumberTitle', 'off'); 
-% hFeat = figure('Name','model initialization', 'NumberTitle', 'off'); 
+% hAlg1 = figure('Name',sprintf('%s', alg(1).strName), 'NumberTitle', 'off');
+% hFeat = figure('Name','model initialization', 'NumberTitle', 'off');
 
 graphCnt = size(listOfFeatureDataFile, 1);
 nodeCnt = 10;
@@ -153,15 +153,15 @@ affinity.GT = zeros(nodeCnt*graphCnt, nodeCnt*graphCnt);
 
 if 0
     for xview = 1:graphCnt-1
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         filePathName1 = fullfile(conf.imgDir, cls, listOfModelDataFile(xview).name );
-
+        
         anno_filePathName1 = fullfile(conf.annoDir, cls, [ listOfModelDataFile(xview).name(1:end-4) '.mat' ]);
-        load(anno_filePathName1, 'pts_coord');%% load the annotated data    
-
+        load(anno_filePathName1, 'pts_coord');%% load the annotated data
+        
         % extract features
         viewInfo1 = extract_localfeatures_mcho( filePathName1, fparam, 'verbose', true );
-
+        
         idx_sel1 = [];
         for i=1:size(pts_coord,2)
             pt1 = pts_coord(1:2,i);
@@ -174,22 +174,22 @@ if 0
         viewInfo1.type = viewInfo1.type( idx_sel1 );
         viewInfo1.frame = viewInfo1.frame( :, idx_sel1);
         viewInfo1.desc = viewInfo1.desc( :, idx_sel1);
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %     feature_filePathName1 = fullfile(listOfFeatureDataFile(xview).folder, listOfFeatureDataFile(xview).name);
-    %     load(feature_filePathName1, 'cdata');
-    %     viewInfo1 = cdata.view;
-    %     clear cdata;
-
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %     feature_filePathName1 = fullfile(listOfFeatureDataFile(xview).folder, listOfFeatureDataFile(xview).name);
+        %     load(feature_filePathName1, 'cdata');
+        %     viewInfo1 = cdata.view;
+        %     clear cdata;
+        
         for yview = xview+1:graphCnt
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            filePathName2 = fullfile(conf.imgDir, cls, listOfModelDataFile(yview).name ); 
-
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            filePathName2 = fullfile(conf.imgDir, cls, listOfModelDataFile(yview).name );
+            
             anno_filePathName2 = fullfile(conf.annoDir, cls, [ listOfModelDataFile(yview).name(1:end-4) '.mat' ]);
             load(anno_filePathName2, 'pts_coord');%% load the annotated data
-
+            
             % extract features
             viewInfo2 = extract_localfeatures_mcho( filePathName2, fparam, 'verbose', true );
-
+            
             idx_sel2 = [];
             for j=1:size(pts_coord,2)
                 pt2 = pts_coord(1:2,j);
@@ -202,45 +202,45 @@ if 0
             viewInfo2.type = viewInfo2.type( idx_sel2 );
             viewInfo2.frame = viewInfo2.frame( :, idx_sel2);
             viewInfo2.desc = viewInfo2.desc( :, idx_sel2);
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %         feature_filePathName2 = fullfile(listOfFeatureDataFile(yview).folder, listOfFeatureDataFile(yview).name);
-    %         load(feature_filePathName2, 'cdata');
-    %         viewInfo2 = cdata.view;
-    %         clear cdata;
-
-             %% make it into cdata
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %         feature_filePathName2 = fullfile(listOfFeatureDataFile(yview).folder, listOfFeatureDataFile(yview).name);
+            %         load(feature_filePathName2, 'cdata');
+            %         viewInfo2 = cdata.view;
+            %         clear cdata;
+            
+            %% make it into cdata
             cdata.fparam = fparam;  cdata.aparam = aparam;  cdata.mparam = mparam;
             cdata.view(1) = viewInfo1;  cdata.view(2) = viewInfo2;
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             cdata2.fparam = fparam;  cdata2.aparam = aparam;  cdata2.mparam = mparam;
             cdata2.view(1) = viewInfo2;  cdata2.view(2) = viewInfo1;
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+            
             %% visualize it
-    %         imgInput = appendimages( cdata.view(1).img, cdata.view(2).img );
-    %         imgInput = double(imgInput)./255;
-    %         offset_x = size(cdata.view(1).img,2);
-    %         set(0, 'CurrentFigure', hFeat); clf;
-    %         %figure('Name','model initialization', 'NumberTitle', 'off'); 
-    %         imshow(rgb2gray(imgInput)); hold on;
-    %         visualizeFeatures(cdata.view(1).frame, 'style', 'point', 'colorcode', 'g');
-    %         visualizeFeatures(cdata.view(2).frame, 'style', 'point', 'colorcode', 'g', 'offset', [offset_x 0] );
-
+            %         imgInput = appendimages( cdata.view(1).img, cdata.view(2).img );
+            %         imgInput = double(imgInput)./255;
+            %         offset_x = size(cdata.view(1).img,2);
+            %         set(0, 'CurrentFigure', hFeat); clf;
+            %         %figure('Name','model initialization', 'NumberTitle', 'off');
+            %         imshow(rgb2gray(imgInput)); hold on;
+            %         visualizeFeatures(cdata.view(1).frame, 'style', 'point', 'colorcode', 'g');
+            %         visualizeFeatures(cdata.view(2).frame, 'style', 'point', 'colorcode', 'g', 'offset', [offset_x 0] );
+            
             %% perform matching
             res = wrapper_matchingBasic( alg(1), cdata );
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             res2 = wrapper_matchingBasic( alg(1), cdata2 );
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %     fprintf('%10s - score:%.5f (%.5f), %.3fsec \n', alg(1).strName, res.score_raw, res.score_GM, res.time);   
-            fprintf('%10s - score:%.5f (%.5f), xxxsec \n', alg(1).strName, res.score_raw, res.score_GM);      
-
+            %     fprintf('%10s - score:%.5f (%.5f), %.3fsec \n', alg(1).strName, res.score_raw, res.score_GM, res.time);
+            fprintf('%10s - score:%.5f (%.5f), xxxsec \n', alg(1).strName, res.score_raw, res.score_GM);
+            
             %% generate raw matrix
-    %         mask = target.pairwiseMask{testk}(xscope, yscope);
+            %         mask = target.pairwiseMask{testk}(xscope, yscope);
             affinity.K{xview, yview} = res.affinityMatrix;
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             affinity.K{yview, xview} = res2.affinityMatrix;
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+            
             pairMatch = zeros(nodeCnt, nodeCnt);
             match = res.cand_matchlist(:,find(res.X));
             for pairIndex = 1:size(match, 2)
@@ -248,35 +248,35 @@ if 0
                 node2 = match(2, pairIndex);
                 pairMatch(node1, node2) = 1;
             end
-
+            
             xscope = (xview-1)*nodeCnt+1:xview*nodeCnt;
             yscope = (yview-1)*nodeCnt+1:yview*nodeCnt;
             rawMat(xscope, yscope) = pairMatch;
-
+            
             %% generate ground truth affinity.GT
             conf.affinity.GTDir = 'C:/Users/xzh87/OneDrive/Reference/Lab/code/IMGM/data/ground_truth';
             xname = listOfModelDataFile(xview).name(end-7:end-4);
             yname = listOfModelDataFile(yview).name(end-7:end-4);
-            GTfile = fullfile(conf.affinity.GTDir, cls, [cls '_' xname '_' yname '.mat'] ); 
+            GTfile = fullfile(conf.affinity.GTDir, cls, [cls '_' xname '_' yname '.mat'] );
             load(GTfile, 'groundTruth');
             affinity.GT(xscope, yscope) = groundTruth.assign;
-
+            
             %% visualize matching
-    %         set(0, 'CurrentFigure', hAlg1); clf;
-    %         %figure('Name',sprintf('%s', alg(1).strName), 'NumberTitle', 'off'); 
-    %         imshow(rgb2gray(imgInput)); hold on;
-    %         colorCode = makeColorCode(nnz(res.X));
-    % 
-    %         visualizeMatches( cdata.view(1).frame, cdata.view(2).frame, res.cand_matchlist(:,find(res.X)), ...
-    %             'offset', [size(cdata.view(1).img,2) 0], 'style', 'frame', 'colorcode', colorCode, 'weight', res.X_raw(find(res.X)) ); 
-
-    %         pause;
+            %         set(0, 'CurrentFigure', hAlg1); clf;
+            %         %figure('Name',sprintf('%s', alg(1).strName), 'NumberTitle', 'off');
+            %         imshow(rgb2gray(imgInput)); hold on;
+            %         colorCode = makeColorCode(nnz(res.X));
+            %
+            %         visualizeMatches( cdata.view(1).frame, cdata.view(2).frame, res.cand_matchlist(:,find(res.X)), ...
+            %             'offset', [size(cdata.view(1).img,2) 0], 'style', 'frame', 'colorcode', colorCode, 'weight', res.X_raw(find(res.X)) );
+            
+            %         pause;
         end
     end
-
+    
     rawMat = rawMat + rawMat' + eye(nodeCnt*graphCnt,nodeCnt*graphCnt);
     
-%     save('rawdata.mat');
+    %     save('rawdata.mat');
     save('rawMat.mat', 'rawMat');
     save('affinity.mat', 'affinity');
 else
